50f7d78ff184e6ed9264042e60831332aa699a5f
sujitrs <suryawanshisujeet@gmail.com>
2019-06-17 13:29:16 +0530

    init

---
+++ src/main/java/com/example/demoCloudSQL/Controller.java
@@ -0,0 +1,10 @@
+package com.example.demoCloudSQL;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	
+	@GetMapping("/get")
+}
---
+++ src/main/java/com/example/demoCloudSQL/DemoCloudSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCloudSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCloudSqlApplication.class, args);
+	}
+
+}
